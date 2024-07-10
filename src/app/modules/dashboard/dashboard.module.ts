ad1d3183672704bfeb25e6fea40de95cd545abd0
Luis Angel Florez Palacio <braian1k3@gmail.com>
2024-07-10 18:25:20 -0500

    feat: new navbar and sidebar.

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
-import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { RouterModule } from '@angular/router';
     HomeComponent],
   imports: [
     CommonModule,
-    RouterModule
+    SharedModule
   ],
 })
 export class DashboardModule {}
